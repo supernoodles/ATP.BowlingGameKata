bff78b5beda9d6dd5a10f7a2507726a44f59209e
Steve Cusack <stephen.cusack@mandg.com>
2019-05-14 11:58:21 +0100

    refactor

--- ATP.BowlingGameKataTests/BowlingGameScoreCalculatorShould.cs
+++ ATP.BowlingGameKataTests/BowlingGameScoreCalculatorShould.cs
@@ -22,39 +22,13 @@
             result.Should().Be(0);
         }
 
-        [Test]
-        public void Return1_Given1PinOnFirstThrowOnFirstFrame()
-        {
-            var result = _calculator.ScoreGame("1-|--|--|--|--|--|--|--|--|--||--");
-            result.Should().Be(1);
-        }
-
-        [Test]
-        public void Return4_Given4PinsOnFirstThrowOnFirstFrame()
-        {
-            var result = _calculator.ScoreGame("4-|--|--|--|--|--|--|--|--|--||--");
-            result.Should().Be(4);
-        }
-
-        [Test]
-        public void Return9_Given9PinsOnFirstThrowOnFirstFrame()
-        {
-            var result = _calculator.ScoreGame("9-|--|--|--|--|--|--|--|--|--||--");
-            result.Should().Be(9);
-        }
-
-
         [TestCase("9-|--|--|--|--|--|--|--|--|--||--", ExpectedResult = 9)]
-
+        [TestCase("1-|--|--|--|--|--|--|--|--|--||--", ExpectedResult = 1)]
+        [TestCase("4-|--|--|--|--|--|--|--|--|--||--", ExpectedResult = 4)]
         public int ReturnExpectedScore_Given1HitOnFirstThrowOnFirstFrame(string gameBoard)
         {
             return _calculator.ScoreGame(gameBoard);
         }
-
-
-
-
-
     }
 }
 
